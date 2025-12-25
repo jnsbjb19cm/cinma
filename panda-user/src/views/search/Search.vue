2c0a9247a429b71fba39a6a25f1dec81ce88a42c
JNSBJB19cm <tianqiag_el@163.com>
2025-12-25 20:30:16 +0800

    更新了搜索错误的问题

--- panda-user/src/views/search/Search.vue
+++ panda-user/src/views/search/Search.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b2c6c433fb6f05137fec818bb56e3a4fd4623e3862f6c78a484524e9512f4f2
-size 1977
+oid sha256:cc0eb5e49c734c0810719a80ebd31b4ccba93c2480181307f6d6538334bddcf5
+size 1981
